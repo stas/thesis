713638e33504f01ffd4fa4aebf62502b2b06ed10
Stas SUȘCOV <stas@net.utcluj.ro>
2015-08-22 23:54:20 +0300

    Updated builder.

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-pandoc --latex-engine=xelatex -H ./tex/settings.tex -V fontsize=12pt \
-  -V documentclass:book -V papersize:a4paper -V classoption:openright \
+pandoc \
+  --latex-engine=xelatex \
+  -H ./tex/settings.tex \
+  -V fontsize=11pt \
+  -V documentclass:book \
+  -V papersize:a4paper \
+  -V classoption:openright \
+  -V classoption:twoside \
   --chapters \
   before/*.md \
   chapter_*/* \
